020364149c95f69ada49a133d27840cf4bef5e7d
Deepanshu <deepanshurajput005@gmail.com>
2022-08-17 12:19:12 +0530

    App Completed and ready to use

--- lib/api/fetch_weather.dart
+++ lib/api/fetch_weather.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
-
 import 'package:weather/model/weather/weather_current_data.dart';
 import 'package:weather/model/weather/weather_data.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather/model/weather/weather_data_daily.dart';
+import 'package:weather/model/weather/weather_data_hourly.dart';
 import 'package:weather/utils/api_url.dart';
 
 class FetchWeatherAPI {
@@ -12,8 +13,10 @@ class FetchWeatherAPI {
   Future<WeatherData> processData(lat, lon) async {
     var response = await http.get(Uri.parse(apiURL(lat, lon)));
     var jsonString = jsonDecode(response.body);
-    weatherData = WeatherData(WeatherDataCurrent.fromJson(jsonString));
-
+    weatherData = WeatherData(
+        WeatherDataCurrent.fromJson(jsonString),
+        WeatherDataHourly.fromJson(jsonString),
+        WeatherDataDaily.fromJson(jsonString));
     return weatherData!;
   }
 }
